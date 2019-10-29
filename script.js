65de22f301f22d87ff4220c716e343f7237ca0b8
Nick Day <thenickday@icloud.com>
2019-10-29 19:24:25 -0400

    finalizing changes for submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-$(".jumper").on("click", function( e ) {
-    
+$(".jumper").on("click", function( e ) {    
     e.preventDefault();
-
     $("body, html").animate({ 
         scrollTop: $( $(this).attr('href') ).offset().top 
-    }, 500);
-    
+    }, 500);    
 });
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+}
